171cab543db580f9292082308a1e71e2d2511aa5
ifanz22 <ifanz22@bss.on.ca>
2021-12-14 11:01:56 -0500

    Finished date retrieving setup, preparing to connect with Firebase

--- public/test.js
+++ public/test.js
@@ -1,5 +1,35 @@
+// Setting up the required components from the web links
+import {
+  initializeApp
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  addDoc,
+  getDoc,
+  updateDoc,
+  onSnapshot
+} from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+
+// setting up my web app framework - use your apiKey and info
+const firebaseApp = initializeApp({
+  apiKey: 'AIzaSyBzUFfFbuAwtWVthoS1IWgH9V-l7RA1eJ0',
+  authDomain: 'timezoneavailability.firebaseapp.com',
+  projectId: 'timezoneavailability'
+});
+
+//---------------------------------------------------------------------------
+
+// setting up the fireStore database
+const db = getFirestore();
+let users = [];
+let times = [];
+
 let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let times = [];
+let pollID = "HAPhRjVZ05KlE3BcdiLP";
 
 class Time{
   constructor(start, end){
@@ -20,52 +50,61 @@ document.getElementById("submitTime").onclick = function() { //Submit the form
   addTime();
 }
 
-function addTime(){
+async function addTime(){
   let frm = document.getElementById("form"); //Retrieving the form
   let weekDay = frm.elements["dayOfWeek"].value; //Getting the day of the week
   let startTime = frm.elements["startTime"].value; //Getting the start time
   let endTime = frm.elements["endTime"].value; //Getting the end time
+  let name = frm.elements["name"].value; //Getting the user's name
   let weekDayNum; //A number for the weekday
   for(let i in weekDays){
     if(weekDay == weekDays[i]){
-      weekDayNum = Number(i+1); //Getting the number corresponding to the weekday
+      weekDayNum = Number(i+1); //Getting the number corresponding to the weekday, Sunday = 1
     }
   }
-  let startHour = 0;
-  if(startTime.substring(6, 8) == "PM"){
-    startHour = Number(startTime.substring(0, 2)) + 11;
-    console.log(startHour);
+  let startHour;
+  if(startTime.substring(6, 8) == "PM"){ //If the time is in the afternoon
+    startHour = Number(startTime.substring(0, 2)) + 11; //Add 11 to it so that it goes to the afternoon
+    //Adding 11 because the hours start at 0 rather than 1
   }else{
-    startHour = Number(startTime.substring(0, 2)) - 1;
-    console.log(startHour);
+    startHour = Number(startTime.substring(0, 2)) - 1; //Otherwise (morning) then minus 1, since hours start at 1 in Javascript
   }
-  let startMinutes = startTime.substring(3, 5);
+  let startMinutes = startTime.substring(3, 5); //Getting the minutes information
 
-  let endHour = 0;
-  if(endTime.substring(6, 8) == "PM"){
+  let endHour;
+  if(endTime.substring(6, 8) == "PM"){ //Same as above but for the end time
     endHour = Number(endTime.substring(0, 2)) + 11;
-    console.log(endHour);
   }else{
     endHour = Number(endTime.substring(0, 2)) - 1;
-    console.log(endHour);
   }
   let endMinutes = endTime.substring(3, 5);
 
-  let start = new Date();
-  console.log(start);
-  start.setFullYear(2017);
-  start.setMonth(0);
-  start.setDate(weekDayNum);
-  start.setHours(startHour);
-  start.setMinutes(startMinutes);
+  let start = new Date(); //New date object
+  start.setFullYear(2017); //Setting the year to 2017, because that is the year when January 1st is on a Sunday
+  start.setMonth(0); //Setting the month to January
+  start.setDate(weekDayNum); //Setting the day of the month, 1-7, since 1 = Sunday
+  start.setHours(startHour); //Setting the hour
+  start.setMinutes(startMinutes, 0); //Setting the minutes
   console.log(start);
 
-  let end = new Date();
-  console.log(end);
+  let end = new Date(); //Same as above but for end time
   end.setFullYear(2017);
   end.setMonth(0);
   end.setDate(weekDayNum);
   end.setHours(endHour);
-  end.setMinutes(endMinutes);
+  end.setMinutes(endMinutes, 0);
   console.log(end);
+
+  db.collection("Polls").doc(pollID).collection(name).add({
+    start: start,
+    end: end
+  });
+
+  // Add a new document in a collection for the user under the poll ID
+  await addDoc(collection(db, "Polls", "HAPhRjVZ05KlE3BcdiLP", name), {
+    start: start,
+    end: end
+  });
+  document.getElementById("dateForm").style.visibility = "hidden";
+  location.reload();
 }
